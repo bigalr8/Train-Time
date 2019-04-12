4973aa4457bd703549218235252a8068cc6aa849
bigalr8 <bigalr8@icloud.com>
2019-04-11 23:25:19 -0400

    attempt to correct next to arrive computation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Train-Time
 Firebase and Moment.js Assignment using Train Schedule
+
+App accepts train schedule entries, places them in a Firebase database and retrieves them for listing in a schedule. 
+The app should determine the time of the next train to arrive, based on the initial daily departure and the specified frequency for it.
+This sould be accomplished using the Moment.js date and time processing javascript library. 
+
+I was not able to sucessfully calculate the next arrovqal time using Momenent. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 // Javascript
 
-// Firebase database configuration snipet
+// F i r e b a s e    d a t a b a s e configuration snipet
 var config = {
   apiKey: "AIzaSyBTviDz97Y1AzZ-oiGeOrC-QRVi4cD_5kY",
   authDomain: "train-time-9192a.firebaseapp.com",
@@ -22,27 +22,28 @@ var destination = "";
 var initlDeparture = "";
 var frequency = 0;
 
+
+// W a i t   f o r   p a g e   t o    l o a d 
 $(document).ready(function () {
 
 
+  // O n   cl i c k   o f   s c h e d u l e   e n t r y    s u b m i s s i o n
   $("#submit").on("click", function (event) {
 
-    console.log("on click");
 
+    // S u p p r e s s   a c t u a l   o f   i n p u t   a s   " f o r m"
     event.preventDefault();
 
-    //Create database variable
+    //S t o r e   e n t e r e d   d a t a   v a l u e   f r o m   i n p u t      
     var trainName = $("#train-name").val().trim();
     var destination = $("#destination").val().trim();
     var initlDeparture = $("#initial-departure").val().trim();
     var frequency = $("#frequency").val();
     var validSched = true;
 
-    console.log("on click: " + trainName);
-    console.log("on click: " + destination);
-    console.log("on click: " + initlDeparture);
-    console.log("on click: " + frequency);
 
+    // F u n c t i o n   t o   v a l i d a t e   i n p u t   
+    //-   s e t t i n g   a n   i n d i c a t o r   i f   i n v al i d  
     function validateAddedSchedule() {
       console.log("validAddedSchedule");
       if (trainName == "") {
@@ -63,51 +64,64 @@ $(document).ready(function () {
       }
     };
 
+    // F u n c t i on   t o   v a l i d a t e   m i l i t a r y   t i m e   o f   i n i t i a l   d e p a r  t u r e  
+    //-   s e t t i n g   a n   i n d i c a t o r   i f   i n v a l i d   
     function checkMilTime(str) {
       console.log("checkMilTime");
+      
       var str = initlDeparture;
+      console.log("str: " + str);
+      console.log("str.len : " + str.length);
+      console.log("validSched: " + validSched);
+
       if (str.length == 0)
-        validSched = false;
-
-      if (str.length < 4)
-        validSched = false;
-
+        {validSched = false;
+         return;
+        }
+      // Must be HH:MM and include any leading zeros
+      if (str.length < 5)
+        {validSched = false;
+          return}
+      // Must include : separator    
       var x = str.indexOf(":");
       if (x < 0) {
         console.log("no :");
         validSched = false;
+        return;
       }
+      // Hours must be between 0 and 24
       else if (
         (str.substr(0, 2) >= 0) &&
         (str.substr(0, 2) <= 24) &&
+
+      // Minutes must be between 00 and 59
         (str.substr(3, 2) >= 0) &&
         (str.substr(3, 2) <= 59) &&
         (str.substr(0, 2) < 24 || (str.substr(0, 2) == 24 && str.substr(3, 2) == 0))
       ) {
-        validSched = true;
-        console.log("checkMilTime true");
+        return;
       }
       else {
         validSched = false;
-        console.log(str.substr(0, 2));
-        console.log(str.substr(3, 2));
-        console.log("checkMilTime false");
+        return;
       }
-      if (!validSched)
-        alert("Initial departure time must be in valid hh:mm format and include any leading zeros");
+      
+        
     };
 
-    validateAddedSchedule();
-    checkMilTime();
-
+    //C a l l   i n p u t   v a l i d a t i o n   f u n c t i o n s 
+        checkMilTime();
     if (!validSched) {
-      console.log("invalid input");
+      console.log("return: " + validSched);
+      alert("Initial departure time must be in valid hh:mm format and include any leading zeros");
+        return;
+    }
+    validateAddedSchedule();
+    if (!validSched) {
       return;
     }
 
-    console.log("no validation exit");
-
-
+    // I n s e r t   v a l i d a t e d   e n t r y   i n t o   d a t a b a s e   a s   n e w    c h i l d  
     database.ref().push({
       trainName: trainName,
       destination: destination,
@@ -115,39 +129,41 @@ $(document).ready(function () {
       frequency: frequency,
       dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
-    console.log("Check database for data!");
+    
   });
 
 
 
-
-  /*
-      row.append(addData(name)).append(addData(role)).append(addData(start)).append(addData(monthsWorked)).append(addData(rate)).append(addData(totalBilled));
-    
-      function addData(data) {
-        var td = $("<td>");
-        td.text(data);
-        return td;
-      }
-    
-      $("#scheduleTrains").append(row);
-    }
-  */
+//W a t c h   d a t a b a s e   f o r   u p d a t e s  
   database.ref().on("child_added", function (snapshot) {
     var trainName = snapshot.val().trainName;
     var destination = snapshot.val().destination;
     var initlDeparture = snapshot.val().initlDeparture;
     var frequency = snapshot.val().frequency;
+
+//C o m p u t e   n e x t   d e p a r t u r e   t i m e  
+//Use "moment" library for date formatting and computations
     var initlDeparture12 = (moment(initlDeparture, 'HH:mm').format('hh:mm a'));
-    var nextToArrive = (moment(initlDeparture12, 'hh:mm a').add(frequency, 'minutes').format('hh:mm a'));
+//    var nextToArrive = (moment(initlDeparture12, 'hh:mm a').add(frequency, 'minutes').format('hh:mm a'));
 
-    console.log("child added-train: " + trainName);
-    console.log("child added-dest: " + destination);
-    console.log("child added-depart: " + initlDeparture);
-    console.log("child added-depart 12: " + initlDeparture12);
-    console.log("child added-freq: " + frequency);
-    console.log("child-added-next:" + nextToArrive);
+    var current = moment().format("hh:mm a");
+    console.log("current: " + current);
 
+    var nextToArrive = initlDeparture12; 
+    console.log("initl next: " + nextToArrive);
+
+    console.log("freq: " + frequency);
+
+
+
+    while ((moment(nextToArrive, 'hh:mm a')).add(frequency, 'minutes').isBefore(moment().format("hh:mm a"))) {
+      next = (moment(nextToArrive, 'hh:mm a').add(frequency, 'minutes').format('hh:mm a'));
+      console.log("next: " + nextToArrive);
+    }
+      console.log("final: " + nextToArrive); 
+      
+
+  //build array of data elements   
     var entryArray = [];
 
     entryArray.push(trainName);
@@ -156,13 +172,18 @@ $(document).ready(function () {
     entryArray.push(frequency);
     entryArray.push(nextToArrive);
 
+    //C a l l   f u n c t i o n   t o   a d d   t o   l i s t   o n   p a g e 
     addEntry(entryArray);
-
+    
+    //Function to add to table on page using array of data elements
     function addEntry(entryArray) {
       var entry = $("<tr>");
+
+    //Use "forEach" method to loop thru array using function to generate html for table
       entryArray.forEach(myfunction);
       $("#scheduleEntries").append(entry);
 
+    //Function to generate table data for each element in array
       function myfunction(item) {
         console.log("addEntry: " + item);
         var data = $("<td></td>").text(item);
